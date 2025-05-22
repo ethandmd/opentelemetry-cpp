35264d155ea079f90b52ddd306ac52e25696e55d
matthewferrari-openai <matthew.ferrari@openai.com>
2025-05-22 07:57:38 -0700

    Update sync_metric_storage.cc

--- sdk/src/metrics/state/sync_metric_storage.cc
+++ sdk/src/metrics/state/sync_metric_storage.cc
@@ -30,7 +30,7 @@ bool SyncMetricStorage::Collect(CollectorHandle *collector,
                                 opentelemetry::common::SystemTimestamp collection_ts,
                                 nostd::function_ref<bool(MetricData)> callback) noexcept
 {
-  OTEL_INTERNAL_LOG_WARN("SyncMetricStorage::Collect");
+  // OTEL_INTERNAL_LOG_WARN("SyncMetricStorage::Collect");
   // Add the current delta metrics to `unreported metrics stash` for all the collectors,
   // this will also empty the delta metrics hashmap, and make it available for
   // recordings
